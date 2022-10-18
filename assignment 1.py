042ae72c59a6fb2682e3b322f7b96c58ddc80f5a
Anurag301 <115408889+Anurag301@users.noreply.github.com>
2022-10-18 15:44:08 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a,b=0,1
+
+while b<50:
+    print(b)
+    a,b = b,a+b
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
